9f6aed8d949c905c4432ef43ab4eefa5030366ea
nin3439 <ninok33_94@mail.ru>
2021-03-28 14:22:32 +0300

    fix: change router

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { Grid } from '@material-ui/core';
 import Header from './Header';
 import { Countries } from '../components/content/Countries';
